4eb2076996d7899948c64b595b0904d42f1b3980
Baoaxid <thaitieubao2004@gmail.com>
2024-07-03 03:18:11 +0700

    finish get class route

--- src/models/Class.js
+++ src/models/Class.js
@@ -4,7 +4,30 @@ const connectDB = require("../config/db");
 class Classroom {
   static async getAllClass() {
     const connection = await connectDB();
-    const result = await connection.request().query(`SELECT * FROM Classes`);
+    const result = await connection.request().query(`SELECT 
+              c.classID,
+              c.className,
+              c.videoLink,
+              c.subject,
+              c.tutorID,
+              t.userID,
+              u.fullName AS tutorFullName,
+              c.studentID,
+              c.paymentID,
+              c.length,
+              c.available,
+              c.type,
+              c.description,
+              c.price,
+              t.rating,
+              c.isActive
+            FROM 
+              Classes c
+            JOIN 
+              Tutors t ON c.tutorID = t.tutorID
+            JOIN 
+              Users u ON t.userID = u.userID 
+            WHERE c.isActive = 1;`);
     return result.recordset;
   }
 
