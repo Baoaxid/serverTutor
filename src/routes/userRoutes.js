5adf8d13de3e02107fe15f4c793d986946026256
Baoaxid <thaitieubao2004@gmail.com>
2024-07-03 11:51:34 +0700

    change get class

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.put("/update/:id", userController.updateUserForUser);
 router.get("/getClass/:id?", classController.getClass);
+router.get("/getAllClass", classController.getAllClass);
 router.post("/complain", userController.sendComplains);
 
 module.exports = router;
