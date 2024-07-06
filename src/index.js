2d1a8566fc854d3b6fe25d114e0763ff4ee4e6a4
Baoaxid <thaitieubao2004@gmail.com>
2024-07-06 14:32:19 +0700

    complete payment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const PayOS = require("@payos/node");
 
 const authRoutes = require("./routes/auth");
 const studentRoutes = require("./routes/studentRoutes");
 const tutorRoutes = require("./routes/tutorRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const userRoutes = require("./routes/userRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 const connectDB = require("./config/db");
 const { Server } = require("socket.io");
 const http = require("http");
-const Message = require("./models/Message");
 
 dotenv.config();
 
@@ -35,6 +34,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/tutors", tutorRoutes);
+app.use("/api", paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -48,7 +48,6 @@ io.on("connection", (socket) => {
   console.log("A user connected");
 
   socket.on("sendMessage", async (message) => {
-    const { senderID, receiverID, messageText } = message;
     try {
       io.emit("receiveMessage", message);
     } catch (error) {
@@ -59,33 +58,4 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
-});
-const payos = new PayOS(
-  "20aad468-aaa1-4e74-ac05-6a56d669e909",
-  "9f4d1792-eb88-4fb3-9184-6b4d61e9736f",
-  "d585d408f91635fb51c818d68bc00834c8f54c3b1b417cb1f5ed64e6ac87eca8"
-); // client_id, api_key, checksum_key
-
-app.post("/create-payment", async (req, res) => {
-  const order = {
-    orderCode: 2,
-    amount: 1000,
-    description: "Thanh toan don hang",
-    items: [
-      {
-        name: "Mì d hảo hảo ly",
-        quantity: 1,
-        price: 1000,
-      },
-    ],
-    cancelUrl: "http://localhost:5000/api/users/getAllClass",
-    returnUrl: "http://localhost:5000",
-  };
-
-  try {
-    const paymentLink = await payos.createPaymentLink(order);
-    res.status(303).json(paymentLink.checkoutUrl);
-  } catch (error) {
-    res.status(500).json({ error: "Error creating payment link" });
-  }
 });
